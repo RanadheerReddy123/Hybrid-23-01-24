74d7b328cf4d4dbe8c2ed383f3919d843eebb831
RanadheerReddy <ranadheerreddykrr@gmail.com>
2024-01-22 20:15:55 +0530

    from new laptop

---
+++ WebDriver_Project/src/sep11/Demo4.java
@@ -0,0 +1,13 @@
+package sep11;
+
+public class Demo4 extends Methods{
+	
+
+	public static void main(String[] args) {
+		Methods.addition(786786, 5675);
+		String result =Methods.verifyStrings("Hello", "hello");
+		System.out.println(result);
+
+	}
+
+}
